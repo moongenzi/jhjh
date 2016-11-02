6c59a8d7e9c58f6fcc3ecb58b18d658fcf73a45b
moongenzi <jaeho0305@naver.com>
2016-11-02 13:37:38 +0900

    Add files via upload

---
+++ jh/sketch.js
@@ -0,0 +1,29 @@
+function setup() {
+  createCanvas(600, 400)
+}
+
+function draw() {
+}
+
+function mouseDragged() {
+  ellipse(mouseX, mouseY, 10, 10);
+  noStroke();
+
+}
+
+function keyPressed() {
+  var r = random();
+  var g = random();
+  var b = random();
+  
+  r = map(r, 0, 1, 0, 255);
+  g = map(g, 0, 1, 0, 255);
+  b = map(b, 0, 1, 0, 255);
+  
+  background(r, g, b);
+  console.log('r' + r);
+  console.log('g' + g);
+  console.log('b' + b);
+  
+
+}
